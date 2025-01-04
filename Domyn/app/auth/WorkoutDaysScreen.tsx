31e2698ccd9c7a2f334f525e2779f1be4a71dbb1
henriceriocain <hceriocain@gmail.com>
2025-01-03 20:32:46 -0500

    Working on WorkoutDaysScreen

--- Domyn/app/auth/WorkoutDaysScreen.tsx
+++ Domyn/app/auth/WorkoutDaysScreen.tsx
@@ -3,10 +3,28 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useUserContext } from '../../hooks/useUserContext';
 
+// This intitialises the 'Days' object, thats a Typscript type. Types defines the parameter of a datastructures
+type Days = {
+  Mon: boolean;
+  Tues: boolean;
+  Wed: boolean;
+  Thu: boolean;
+  Fri: boolean;
+  Sat: boolean;
+  Sun: boolean;
+};
+
 export default function WorkoutDaysScreen() {
+
   const router = useRouter();
+
+  // This destructures the name, and addWorkout properties of UserContext
+  // useUserContext is a React hook that allows us to use UserContext, its values and its methods
+  // Whats a hook? 
+  //    A special function that makes remembering data easier.
   const { name, addWorkout } = useUserContext();
-  const [days, setDays] = useState({
+  // This sets all Days to be by default false. Consts are constant variables where values cannot be changed.
+  const [days, setDays] = useState<Days>({
     Mon: false,
     Tues: false,
     Wed: false,
@@ -15,15 +33,15 @@ export default function WorkoutDaysScreen() {
     Sat: false,
     Sun: false,
   });
-
-  const handleDayPress = (day) => {
+  // Defines a const function, input is a day of a Days object
+  const handleDayPress = (day: keyof Days) => {
     setDays((prevDays) => ({ ...prevDays, [day]: !prevDays[day] }));
   };
 
   const handleContinue = () => {
-    const selectedDays = Object.keys(days).filter((day) => days[day]);
+    const selectedDays = Object.keys(days).filter((day) => days[day as keyof Days]);
     if (selectedDays.length > 0) {
-      selectedDays.forEach((day) => addWorkout(day));
+      selectedDays.forEach((day) => addWorkout(day as keyof Days));
       router.push({
         pathname: './WorkoutDetailsScreen',
         params: { selectedDays },
@@ -47,8 +65,8 @@ export default function WorkoutDaysScreen() {
         {Object.keys(days).map((day) => (
           <TouchableOpacity
             key={day}
-            style={[styles.dayBox, days[day] && styles.daySelected]}
-            onPress={() => handleDayPress(day)}
+            style={[styles.dayBox, days[day as keyof Days] && styles.daySelected]}
+            onPress={() => handleDayPress(day as keyof Days)}
           >
             <Text style={styles.dayBoxText}>{day}</Text>
           </TouchableOpacity>
