dce78d2014fec6496456746e3a02b27c699e0e64
jinawalbourne <jn951681@dal.ca>
2025-01-04 23:19:13 -0800

    Fixed FaqScreen display

--- Domyn/app/settings/FaqScreen.tsx
+++ Domyn/app/settings/FaqScreen.tsx
@@ -21,15 +21,11 @@ export default function FAQScreen() {
           <Text style={styles.sectionHeader}>1. General</Text>
           <View style={styles.questionBlock}>
             <Text style={styles.question}>Q: What is this app used for?</Text>
-            <Text style={styles.answer}>
-              A: This app helps you track your workouts, monitor progress, and stay motivated.
-            </Text>
+            <Text style={styles.answer}>A: This app helps you track your workouts, monitor progress, and stay motivated.</Text>
           </View>
           <View style={styles.questionBlock}>
             <Text style={styles.question}>Q: Can I use this app to track any type of workout?</Text>
-            <Text style={styles.answer}>
-              A: Yes! This app can be used to track any type of workout, whether it is weightlifting, yoga, cardio, or any type of activity.
-            </Text>
+            <Text style={styles.answer}>A: Yes! This app can be used to track any type of workout, whether it is weightlifting, yoga, cardio, or any type of activity.</Text>
           </View>
         </View>
 
@@ -37,15 +33,11 @@ export default function FAQScreen() {
           <Text style={styles.sectionHeader}>2. Workout Schedule</Text>
           <View style={styles.questionBlock}>
             <Text style={styles.question}>Q: How do I set up my workout dates?</Text>
-            <Text style={styles.answer}>
-              A: When you enter our domain, you'll choose the days you want to work out. Then, you'll be prompted to enter your exercises and any optional weight/reps you want to track.
-            </Text>
+            <Text style={styles.answer}>A: When you enter our domain, you'll choose the days you want to work out. Then, you'll be prompted to enter your exercises and any optional weight/reps you want to track.</Text>
           </View>
           <View style={styles.questionBlock}>
             <Text style={styles.question}>Q: Can I change my workout schedule after setting it?</Text>
-            <Text style={styles.answer}>
-              A: Yes! Navigate to <Text style={styles.highlight}>Settings &gt; Edit Schedule</Text> to edit the days and types of exercises you want.
-            </Text>
+            <Text style={styles.answer}>A: Yes! Navigate to <Text style={styles.highlight}>Settings &gt; Edit Schedule</Text> to edit the days and types of exercises you want.</Text>
           </View>
         </View>
 
@@ -53,15 +45,11 @@ export default function FAQScreen() {
           <Text style={styles.sectionHeader}>3. Streaks/Progress</Text>
           <View style={styles.questionBlock}>
             <Text style={styles.question}>Q: How do streaks work?</Text>
-            <Text style={styles.answer}>
-              A: Streaks track the number of consecutive workout days you complete. Missing a scheduled day will restart your streak to 0.
-            </Text>
+            <Text style={styles.answer}>A: Streaks track the number of consecutive workout days you complete. Missing a scheduled day will restart your streak to 0.</Text>
           </View>
           <View style={styles.questionBlock}>
             <Text style={styles.question}>Q: Do rest days restart my streak?</Text>
-            <Text style={styles.answer}>
-              A: No. Streaks will continue as long as no planned workout days are skipped. Rest days will not count towards streak days.
-            </Text>
+            <Text style={styles.answer}>A: No. Streaks will continue as long as no planned workout days are skipped. Rest days will not count towards streak days.</Text>
           </View>
         </View>
 
@@ -108,13 +96,13 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   question: {
-    fontSize: 18,
+    fontSize: 19,
     fontWeight: "bold",
     color: "white",
     marginBottom: 15,
   },
   answer: {
-    fontSize: 16,
+    fontSize: 17,
     color: "white",
     lineHeight: 22,
   },
